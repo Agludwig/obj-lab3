78cc46361107ada6631c69e0461aa82318811f91
Agludwig <ludwigja@chalmers.se>
2026-02-17 02:39:18 +0100

    Delete src directory

--- src/main/java/Truck.java
+++
@@ -1,9 +0,0 @@
-import java.awt.*;
-
-public class Truck extends LandVehicle {
-
-    public Truck(int nrDoors, Color color, int enginePower, String modelName) {
-        super(nrDoors, color, enginePower, modelName);
-        this.stopEngine();
-    }
-}
