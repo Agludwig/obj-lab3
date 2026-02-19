aa5f52ad1f44da45e80836fd92b367b5ad069abd
Agludwig <ludwigja@chalmers.se>
2026-02-19 02:30:30 +0100

    Delete src directory

--- src/CollisionBehavior.java
+++
@@ -1,7 +0,0 @@
-public interface CollisionBehavior {
-    /**
-     * Called when a car is about to collide with a wall.
-     * Implementations decide how to react (stop, reverse, teleport, etc.).
-     */
-    void onWallCollision(Car car);
-}
--- src/README.md
+++
@@ -1,1 +0,0 @@
-Kan ha baserat ganska mycket på en äldre version av labb 2 och jag vet inte hur die-hard de var med OEP. Därför valde jag att göra studsandet till sin egen klass och interface som hanterar sånt.
